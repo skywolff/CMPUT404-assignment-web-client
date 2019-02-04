87b7296c41b1e7fd7e85f47db457936aa47a65a3
Alex Li <tianyi7@ualberta.ca>
2019-02-04 15:38:40 -0700

    passed free test

--- httpclient.py
+++ httpclient.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 # coding: utf-8
+#
+# Copyright 2019 Alex Li
+#
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,48 +36,129 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
-
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
         return None
 
     def get_code(self, data):
-        return None
+        try:
+            crlf = data.index("\r\n")
+            code = int(data[:crlf].split()[1])
+            return code
+        except:
+            return None
+
 
     def get_headers(self,data):
-        return None
+        try:
+            return data.split("\r\n\r\n")[0]
+        except:
+            return None
 
     def get_body(self, data):
-        return None
-    
+        try:
+            return data.split("\r\n\r\n")[1]
+        except:
+            return None
+
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
-        
+
     def close(self):
         self.socket.close()
 
     # read everything from the socket
     def recvall(self, sock):
-        buffer = bytearray()
-        done = False
-        while not done:
-            part = sock.recv(1024)
-            if (part):
-                buffer.extend(part)
-            else:
-                done = not part
-        return buffer.decode('utf-8')
+        try:
+            buffer = bytearray()
+            done = False
+            while not done:
+                sock.settimeout(1.0)
+                part = sock.recv(1024)
+                if (part):
+                    buffer.extend(part)
+                else:
+                    done = not part
+            return buffer.decode("utf-8")
+        except socket.timeout:
+            return buffer.decode("utf-8")
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        url = urllib.parse.urlparse(url)
+        host,port = url.hostname, url.port
+
+        # create request
+        query = ""
+        if args:
+            query = urllib.parse.urlencode(args)
+        requestline = "GET" + " "
+        if not url.path.startswith("/"):
+            requestline += "/"
+        requestline += url.path
+        requestline += "?" + query + " "
+        requestline += "HTTP/1.1"
+        mediatypes = "*/*"
+        headers = [
+            requestline,
+            "Host: " + host,
+            "Accept: " + mediatypes
+        ]
+        request = "\r\n".join(headers) + "\r\n\r\n"
+
+        # send request
+        if not port:
+            if url.scheme is "http":
+                port = 80
+            elif url.scheme is "https":
+                port = 443
+            else:
+                port = 8080
+        self.connect(host,port)
+        self.sendall(request)
+        data = self.recvall(self.socket)
+        self.close()
+        code, body = self.get_code(data), self.get_body(data)
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        url = urllib.parse.urlparse(url)
+        host, port = url.hostname, url.port
+
+        # create request
+        content = ""
+        if args:
+            content = urllib.parse.urlencode(args)
+        requestline = "POST" + " "
+        if not url.path.startswith("/"):
+            requestline += "/"
+        requestline += url.path + " "
+        requestline += "HTTP/1.1"
+        mediatypes = "*/*"
+        contenttype = "application/x-www-form-urlencoded"
+        headers = [
+            requestline,
+            "Host: " + host,
+            "Accept: " + mediatypes,
+            "Content-Type: " + contenttype,
+            "Content-Length: " + str(len(content))
+        ]
+        request = "\r\n".join(headers)
+        request += "\r\n\r\n" + content + "\r\n\r\n"
+
+        # send request
+        if not port:
+            if url.scheme is "http":
+                port = 80
+            elif url.scheme is "https":
+                port = 443
+            else:
+                port = 8080
+        self.connect(host,port)
+        self.sendall(request)
+        data = self.recvall(self.socket)
+        self.close()
+        code,body = self.get_code(data), self.get_body(data)
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
@@ -82,7 +166,7 @@ class HTTPClient(object):
             return self.POST( url, args )
         else:
             return self.GET( url, args )
-    
+
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
